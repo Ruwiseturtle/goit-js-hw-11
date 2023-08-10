6b4c9fc4fe3007445acc1b2ab49918cb45a23220
Matviienko Ruslana <brauniesrus@gmail.com>
2023-08-10 14:51:32 +0300

    hw11

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
-
-import { fetchBreeds } from './js/functions';
-import { createMarkupPictures } from './js/functions';
+import { fetchBreeds, createMarkupPictures } from './js/functions';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '38710040-e4c1e1eb16f2bb925e73b2921';
@@ -11,7 +9,7 @@ const IMAGE_TYPE = 'photo';
 const ORIENTATION = 'orientation';
 const SAFE_SERACH = 'true';
 let SEARCH_TERM = '';
-let PER_PAGE = '6';
+const PER_PAGE = '4';
 let TOTAL_HITS = 1;
 let PAGE = 1;
 
@@ -20,30 +18,37 @@ const btnLoadMore = document.querySelector('.load-more');
 const list = document.querySelector('ul.gallery');
 const loadMore = document.querySelector('.load-more');
 
-//налаштування для Notflix
-Notiflix.Notify.init({
-  width: '480px',
-  position: 'right-top',
-  distance: '90px',
-  opacity: 1,
-  fontSize: '20px',
-  clickToClose: true,
-  timeout: 3000,
-  // ...
-});
-
-//налаштування для simpleLightBox
+//--------налаштування для simpleLightBox---------
 let lightbox = new SimpleLightbox('.gallery a', {
   caption: true,
   captionsData: 'alt',
   captionDelay: 250,
 });
 
-lightbox.refresh();
+//-----створення та налаштування обсервера --------
+const options = {
+    threshold: 1
+}
+
+const callback = function (entries, observer) {
+    entries.forEach(entry => {
+        const { target, isIntersecting, intersectionRatio } = entry;
+
+        if (isIntersecting) {
+            getApiPictures(PAGE);
+        }
+    })
+}
+
+const observer = new IntersectionObserver(callback, options);
+// const target = document.querySelector('load-more');
+observer.observe(btnLoadMore);
+
+//-------------------------------------------------
+
 
 //слідкувач для кнопки запускає ф-цію getPictures
 form.addEventListener('submit', getPictures);
-loadMore.addEventListener('click', getApiPictures);
 
 //ф-ція з input витягує текст і викликає ф-цію, яка з цим текстом витягує з сервера фото
 function getPictures(e) {
@@ -65,15 +70,12 @@ function getApiPictures(page = 1) {
 
 //якщо дані витягуємо вдало, то кладемо їх в масив
 function renderData(dataPictures) {
-  if (dataPictures.total === 0) {
+  if (dataPictures.total === 0 || SEARCH_TERM ==='') {
     Notiflix.Notify.failure('Немає інформації по цьому запиту!');
     return;
   }
 
   TOTAL_HITS = dataPictures.totalHits;
-  console.log(TOTAL_HITS);
-  console.log(PER_PAGE * PAGE);
-  console.log();
   let markup = createMarkupPictures(dataPictures);
   list.insertAdjacentHTML('beforeend', markup);
 
@@ -92,17 +94,29 @@ function errorfetchData() {
 }
 
 function showBtnLoad() {
-  btnLoadMore.hidden = PER_PAGE * PAGE !== TOTAL_HITS ? false : true;
-  // if (PER_PAGE * PAGE !== TOTAL_HITS) {
-  //   btnLoadMore.hidden = false;
-  // } else {
-  //   btnLoadMore.hidden = true;
-  // }
+  if (PER_PAGE * PAGE !== TOTAL_HITS && TOTAL_HITS >= PER_PAGE) {
+    btnLoadMore.hidden = false;
+  } else {
+    btnLoadMore.hidden = true;
+  }
+  lightbox.refresh();
 }
 
 //ф-ція скидає усі дані і очищає сторінку
 function resetData() {
-  btnLoadMore.hidden = true;
   list.innerHTML = '';
   PAGE = 1;
 }
+
+
+//налаштування для Notflix
+Notiflix.Notify.init({
+  width: '480px',
+  position: 'right-top',
+  distance: '90px',
+  opacity: 1,
+  fontSize: '20px',
+  clickToClose: true,
+  timeout: 3000,
+  // ...
+});
